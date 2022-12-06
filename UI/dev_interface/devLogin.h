9133a0d10e71615bdc833015bb9d67d40201e59b
CaptainPandaMonkey <angelcunanan15@gmail.com>
2022-12-06 22:18:29 +0800

    working on testing, broken on the main; 4 errors

--- UI/dev_interface/devLogin.h
+++ UI/dev_interface/devLogin.h
@@ -1,35 +1,60 @@
 #pragma once
 
-bool devLogin(const Users& users)
+void devLogin(const Users& users)
 {
-    system("cls");
-	std::string usermain;
-    std::string devPassword;
+	string usermain;
+    string devPassword;
     int login_Attempt = 0;
 	
     while (login_Attempt < 3)
     {
-        std::cout << "username: ";
-        std::cin >> usermain;
-        std::cout << std::endl;
-        std::cout << "password: ";
-        std::cin >> devPassword;
+        cout << "username: ";
+        cin >> usermain;
+        cout << endl;
+        cout << "password: ";
+        cin >> devPassword;
 
         if (usermain == "group0" && devPassword == "group01")
         {
-            // log dev in
-            break;
+			system("cls");
+			Users users;
+			unsigned opt {};
+
+			if (!readUsers(users))
+				cout << "PROBLEM READING USERS\n";
+
+			cout << "\n\tWelcome Developer" << endl;
+			do
+			{
+				cout << "\n\t[1] DELETE USER" << endl;
+				cout << "\n\t[2] DISPLAY ALL USERS" << endl;
+				cout << "\n\t[3] EXIT PROGRAM" << endl;
+				
+				switch(opt = getInt("\nENTER CHOICE: "))
+				{
+					case 1:
+						deleteUser(users);
+						break;
+					case 2:
+						displayUsers(users);
+						break;
+					case 3:
+						exit(0);
+						break;
+					default:
+						cout << "Invalid Option" << endl;
+				}
+			}while (opt);
         }
         else
         {
-            std::cout << "INVALID LOGIN ATTEMPT... PLEASE TRY AGAIN!!" << std::endl;
+            cout << "INVALID LOGIN ATTEMPT... PLEASE TRY AGAIN!!" << endl;
             login_Attempt++;
         }
     }
     if (login_Attempt == 3)
     {
-        std::cout << "\n\tTOO MANY LOG IN ATTEMPTS!" << std::endl;
-        std::cout << "\n\tGOING BACK TO MAIN MENU" << std::endl;
+        cout << "\n\tTOO MANY LOG IN ATTEMPTS!" << endl;
+        cout << "\n\tGOING BACK TO MAIN MENU" << endl;
     }
-    return false;
 };
--- UI/dev_interface/password.h
+++ UI/dev_interface/password.h
@@ -1,13 +1,9 @@
 #pragma once
 
-int password() 
-{
+int password() {
 	system("cls");
 	Users users;
 	unsigned opt {};
-	std::string usermain;
-    std::string devPassword;
-    int login_Attempt = 0;
 
 	if (!readUsers(users))
 		cout << "Problem reading users\n";
@@ -16,20 +12,14 @@ int password()
 		cout << "\n\t[1] LOGIN" << endl;
 		cout << "\n\t[2] CHANGE PASSWORD" << endl;
 		cout << "\n\t[3] SIGN UP" << endl;
-		cout << "\n\t[4] DELETE USER" << endl;
-		cout << "\n\t[5] DISPLAY ALL USERS" << endl;
-		cout << "\n\t[6] ATTEMPTED DEV ADD ON" << endl;
+		cout << "\n\t[4] LOG IN FOR DEVELOPERS" << endl;
 		cout << "\n\t[0] EXIT" << endl;
+
 
 		switch (opt = getInt("\nEnter options: ")) {
 			case 3:
 				addUser(users);
 				break;
-
-			case 4:
-				deleteUser(users);
-				break;
-
 			case 2:
 				changePass(users);
 				break;
@@ -40,48 +30,15 @@ int password()
 				else
 					cout << "Invalid\n";
 				break;
-
-			case 5:
-				displayUsers(users);
-				break;
-
-			case 6:
-			// working and tested
-    while (login_Attempt < 3)
-    {
-        std::cout << "username: ";
-        std::cin >> usermain;
-        std::cout << std::endl;
-        std::cout << "password: ";
-        std::cin >> devPassword;
-
-        if (usermain == "group0" && devPassword == "group01")
-        {
-            // log dev in
-            break;
-        }
-        else
-        {
-            std::cout << "INVALID LOGIN ATTEMPT... PLEASE TRY AGAIN!!" << std::endl;
-            login_Attempt++;
-        }
-    }
-    if (login_Attempt == 3)
-    {
-        std::cout << "\n\tTOO MANY LOG IN ATTEMPTS!" << std::endl;
-        std::cout << "\n\tGOING BACK TO MAIN MENU" << std::endl;
-    }
-				break;
-
+      		case 4:
+        		devLogin(users);
+        		break;
 			case 0:
 				if (!writeUsers(users))
 					cout << "Problem writing users\n";
-
 				break;
-
 			default:
 				cout << "Invalid option\n";
 		}
 	} while (opt);
-	return 0;
 };
