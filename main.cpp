246a0b645c741172e36572642ebcfd6ae4b2dd5b
jeffrsnblrdoo <118502737+jeffrsnblrdoo@users.noreply.github.com>
2022-12-09 19:15:17 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,82 +1,124 @@
 #include <iostream>
-#include <string>
-#include <fstream>
-#include <set>
-#include <map>
-#include <sstream>
-#include <iomanip>
-#include <limits>
-#include <stdlib.h>
-#include <conio.h>
 
 using namespace std;
 
-struct UserData {
-	string name;
-	string curpass;
-	set<string> prevpass;
-};
+void inputData(int size, string Q[]) {
+    cout << "\n Books to purchase: " << endl;
+    for (int ctr = 0; ctr < size; ctr++)
+    {
+        cout << " ";
+        getline(cin >> ws, Q[ctr]);
+    }
+}
 
-using Users = map<string, UserData>;
+void display(int size, string Q[]) {
+    double order, discount, total;
 
-enum Passerr {OK, UPPER, LOWER, LENGTH, DUPLICATE, LAST};
+    if(size >= 3){
+        order = 200 * size;
+        discount = 200 * 0.05;
+        total = order - discount;
+    } else {
+        total = 200 * size;
+    }
 
-const string PassErrs[LAST] {"", "Insufficient upper case chars", "Insufficient lower case chars", "Not enough characters", "Duplicate"};
-const string userfile {"users.txt"};
-
-// Format of file is
-#include "UI/getInt.h"
-
-#include "UI/student_interface/readUser.h"
-#include "UI/student_interface/writeUser.h"
-#include "UI/student_interface/checkPass.h"
-#include "UI/student_interface/getUser.h"
-#include "UI/student_interface/getPass.h"
-#include "UI/student_interface/addUser.h"
-#include "UI/student_interface/changePass.h"
-
-#include "UI/dev_interface/displayUsers.h" // list of users - dev TODO: change password output to ****** <- 
-#include "UI/dev_interface/deleteUsers.h"
-#include "UI/dev_interface/password.h" // UI - student && dev TODO: separate the ones for the dev and student
-// create password_student and password_dev ??
-#include "UI/dev_interface/devInfo.h"
+    cout << " These are the books you purchased: " << endl;
+    for (int ctr = 0; ctr < size; ctr++)
+        cout << " " << Q[ctr] << "\tPhp 200.00" << endl;
+    cout << "\n Total:\t\t\t\t\tPhp " << total << ".00" << endl;
+}
 
 int main()
 {
-  int choice;
+    string name, gender, email, phoneNum;
+    int age, studentNum, yearLevel, arrSize;
+    char answer;
 
-  while(1)
-  {
-    cout << "Project in Data Structures and Algorithm (MCS 201)" << endl;
-    cout << "\n\tMAIN MENU:" << endl;
-    cout << "\n\t[1] LOG IN" << endl;
-    cout << "\n\t[2] ABOUT THE DEVELOPERS" << endl;
-    cout << "\n\t[3] EXIT" << endl;
-    cout << "\n\tChoice: ";
-    cin >> choice;
+    while(true) {
+        cout << "--------------------------------------------------" << endl;
+        cout << " Enter your full name: ";
+        getline(cin >> ws, name);
+        cout << " Enter your gender: ";
+        cin >> gender;
+        cout << " Enter your age: ";
+        cin >> age;
+        cout << " Enter your phone number: ";
+        cin >> phoneNum;
+        cout << " Enter your email address: ";
+        cin >> email;
+        cout << " Enter your student number: ";
+        cin >> studentNum;
+        cout << "--------------------------------------------------" << endl;
+        cout << " Name: " << name << endl;
+        cout << " Gender: " << gender << endl;
+        cout << " Age: " << age << endl;
+        cout << " Phone number: " << phoneNum << endl;
+        cout << " Email address: " << email << endl;
+        cout << " Student number: " << studentNum << endl;
 
-    switch(choice)
-    {
-      case 1:
-        password();
-        break;
-      case 2:
-        devInfo();
-        break;
-      case 3:
-        cout << endl;
-        cout << "THANK YOU FOR USING OUR SOFTWARE" << endl;
-        cout << endl;
-        exit(0);
-        break;
-      default:
-        cout << "\n\t YOU ENTERED THE WRONG CHOICE..." << endl;
-        cout << "\n\t PRESS ANY KEY TO TRY AGAIN" << endl;
-        cout << "-----------------------------------------------" << endl;
-        getch();
-        break;
+        cout << "\n Do you want to make changes [Y/N]: ";
+        cin >> answer;
+
+        if(answer == 'y' || answer == 'Y')
+            true;
+        else
+            break;
     }
-    system("cls");
-  }
-  return 0;
-};
+
+    cout << "--------------------------------------------------" << endl;
+    cout << " Enter your year level [1/2/3/4]: ";
+    cin >> yearLevel;
+    cout << "--------------------------------------------------" << endl;
+
+    if(yearLevel == 1) {
+        cout << "               ====BOOK TITLES====" << endl;
+        cout << "\n FIRST YEAR" << endl;
+        cout << " 1. Fundamentals of Information Technology" << endl;
+        cout << " 2. Integrated Computer Applications" << endl;
+        cout << " 3. Learning Guide in C Programming" << endl;
+        cout << "--------------------------------------------------" << endl;
+        cout << " How many books would you like to purchase?: ";
+        cin >> arrSize;
+    } else if(yearLevel == 2) {
+        cout << "               ====BOOK TITLES====" << endl;
+        cout << "\n SECOND YEAR" << endl;
+        cout << " 1. Database Management System" << endl;
+        cout << " 2. Networking" << endl;
+        cout << " 3. Object Oriented Programming" << endl;
+        cout << " 4. Assembly Language" << endl;
+        cout << "--------------------------------------------------" << endl;
+        cout << " How many books would you like to purchase?: ";
+        cin >> arrSize;
+    } else if(yearLevel == 3) {
+        cout << "               ====BOOK TITLES====" << endl;
+        cout << "\n THIRD YEAR" << endl;
+        cout << " 1. Software Engineering" << endl;
+        cout << " 2. SQL" << endl;
+        cout << " 3. Python" << endl;
+        cout << " 4. Web Development" << endl;
+        cout << "--------------------------------------------------" << endl;
+        cout << " How many books would you like to purchase?: ";
+        cin >> arrSize;
+    } else if(yearLevel == 4) {
+        cout << "               ====BOOK TITLES====" << endl;
+        cout << "\n FOURTH YEAR" << endl;
+        cout << " 1. Embedded Systems" << endl;
+        cout << " 2. Wireless Technology" << endl;
+        cout << " 3. Android Programming" << endl;
+        cout << "--------------------------------------------------" << endl;
+        cout << " How many books would you like to purchase?: ";
+        cin >> arrSize;
+    } else {
+        cout << " Invalid Input" << endl;
+        cout << "--------------------------------------------------" << endl;
+    }
+
+    string *num = new string[arrSize];
+
+    inputData(arrSize, num);
+    cout << "------------------------------------------------------" << endl;
+    display(arrSize, num);
+    cout << "------------------------------------------------------" << endl;
+
+    return 0;
+}
