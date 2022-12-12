5ab908e5f220225a782501887519f834b91f0578
MysteryMist <119337625+MysteryMist14@users.noreply.github.com>
2022-12-13 07:43:47 +0800

    Update devInfo.h

--- UI/dev_interface/devInfo.h
+++ UI/dev_interface/devInfo.h
@@ -8,57 +8,28 @@ void devInfo()
 {
     system("cls");
     cout << "ABOUT THE DEVELOPERS AND DEVELOPER STATUS" << endl;
-    cout << "PRESENTED BY:" << endl;
-    cout << "\n\tNeil Allen H. San Diego";
-    cout << "\n\tBSM CS 2A";
-    cout << "\n\t ";
-    cout << "\n\tnsandiego14@gmail.com";
+    cout << "\n\tLeader:" << endl;
+    cout << "\n\tNeil Allen H. San Diego\t\tnsandiego14@gmail.com" << endl;
+    cout << "\n\n\tMembers:";
+    cout << "\n\tVincent G. Enfectana\t\tvintanafanteneca@gmail.com";
+    cout << "\n\tJomari T. Basario\t\tjomarit.basario@gmail.com";
+    cout << "\n\tLars Ulric B. Sabado\t\tlarsulric.sabado.b@bulsu.edu.ph";
+    cout << "\n\tLovely Angeline OL. Cunanan\t\tangelcunanan15@gmail.com" << endl;
+    cout << "\n\tNeil Jefferson A. Belardo\t\tiamjeffersonbelardo@gmail.com" << endl;
+    cout << "\n\tYuya C. Shinohara\t\tyuya.shinohara.c@bulsu.edu.ph" << endl;
+    cout << "\n\tJoshua Cagalingan\t\tjoshua.cagalingan.x@bulsu.edu.ph" << endl;
+    cout << "\n\tJericho John M. Baguisa\t\tjerichojohn.baguisa.m@bulsu.edu.ph" << endl;
+    cout << "\n\tSaimon B. Bello\t\tsaimon.bello.b@bulsu.edu.ph" << endl;
     cout << endl;
-    cout << "\n\tTASK ";
-    cout << "\n\thotdog";
+    cout << "\n\n\tAssigned Tasks:" << endl;
+    cout << "\n\tPresentation:\t\tBelardo";
+    cout << "\n\tCompilation:\t\tSan Diego & Enfectana";
+    cout << "\n\tCompilation/Debugging:\t\tBelardo & Cunanan";
+    cout << "\n\tCustomer Menu:\t\tBello & Shinohara";
+    cout << "\n\tAdmin Menu:\t\tSabado & Basario";
+    cout << "\n\tStocks Menu:\t\tBaguisa & Cagalingan";
     cout << endl;
-
-    cout << "\n\tVincent G. Enfectana";
-    cout << "\n\tBSM CS 2A";
-    cout << "\n\t ";
-    cout << "\n\tvintanafanteneca@gmail.com";
-    cout << endl;
-    cout << "\n\tTASK ";
-    cout << "\n\thotdog";
-    cout << endl;
-
-    cout << "\n\tJomari T. Basario";
-    cout << "\n\tBSM CS 2A";
-    cout << "\n\t ";
-    cout << "\n\tjomarit.basario@gmail.com";
-    cout << endl;
-    cout << "\n\tTASK ";
-    cout << "\n\thotdog";
-    cout << endl;
-
-    cout << "\n\tLars Ulric B. Sabado";
-    cout << "\n\tBSM CS 2A";
-    cout << "\n\t ";
-    cout << "\n\tlarsulric.sabado.b@bulsu.edu.ph";
-    cout << endl;
-    cout << "\n\tTASK ";
-    cout << "\n\thotdog";
-    cout << endl;
-
-    cout << "\n\tLovely Angeline OL. Cunanan";
-    cout << "\n\tBSM CS 2A";
-    cout << "\n\t ";
-    cout << "\n\tangelcunanan15@gmail.com";
-    cout << endl;
-    cout << "\n\tTASK ";
-    cout << "\n\thotdog";
-
-    cout << endl;
-    cout << endl;
-    cout << "PRESENTED TO: " << endl;
-    cout << "\n\tENGR: EVELYN C. SAMSON";
-    cout << "\n\tINSTRUCTOR" << endl; 
-    cout << endl;
+    cout << "PRESENTED TO:\t\tENGR. EVELYN C. SAMSON" << endl;
     cout << "PRESS ANY KEY TO RETURN" << endl;
     getch();
 };
